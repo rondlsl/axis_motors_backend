b538c63686937a1432fb0fddbe271e071084152b
Karina Kim <220103048@stu.sdu.edu.kz>
2025-06-23 14:43:10 +0500

    delete while true

--- main.py
+++ main.py
@@ -152,135 +152,135 @@ def create_mock_cars_for_almaty(db: Session):
 
     db.commit()
 
-#
-# def init_app(app: FastAPI):
-#     @app.on_event("startup")
-#     async def startup_event():
-#         print("🚀 Приложение запущено")
-#         run_migrations()
-#
-#         db_gen = get_db()
-#         db = next(db_gen)
-#         scheduler.add_job(
-#             billing_job,
-#             trigger="interval",
-#             seconds=10,
-#             max_instances=1,  # не запустит новую итерацию, пока старая не завершилась
-#             coalesce=True  # если промедлили — слить «пропущенные» вызовы в один
-#         )
-#         scheduler.start()
-#
-#         try:
-#             owner_phone = "77000250400"
-#             owner = db.query(User).filter(User.phone_number == owner_phone).first()
-#             if not owner:
-#                 owner = User(phone_number=owner_phone, role=UserRole.FIRST, wallet_balance=0)
-#                 db.add(owner)
-#                 db.commit()
-#                 db.refresh(owner)
-#
-#             if not db.query(Car).filter(Car.id == 1).first():
-#                 photos_dir = os.path.join(os.path.dirname(__file__), "uploads", "cars", "1")
-#                 photos = []
-#                 if os.path.isdir(photos_dir):
-#                     for fname in sorted(os.listdir(photos_dir)):
-#                         if os.path.isfile(os.path.join(photos_dir, fname)):
-#                             photos.append(f"/uploads/cars/1/{fname}")
-#
-#                 car1 = Car(
-#                     id=1,
-#                     name="HAVAL F7x",
-#                     gps_id="800153076",
-#                     gps_imei="866011056063951",
-#                     engine_volume=2.0,
-#                     year=2021,
-#                     drive_type=3,
-#                     price_per_minute=70,
-#                     price_per_hour=3125,
-#                     price_per_day=50000,
-#                     plate_number="422ABK02",
-#                     latitude=43.238949,
-#                     longitude=76.889709,
-#                     fuel_level=80,
-#                     owner_id=owner.id,
-#                     course=90,
-#                     description="Машина в идеальном состоянии.",
-#                     photos=photos
-#                 )
-#                 db.add(car1)
-#                 db.commit()
-#                 print("✅ HAVAL F7x (id=1) добавлена")
-#             else:
-#                 print("ℹ️ HAVAL F7x (id=1) уже существует")
-#
-#             if not db.query(Car).filter(Car.id == 2).first():
-#                 photos_dir = os.path.join(os.path.dirname(__file__), "uploads", "cars", "2")
-#                 photos = []
-#                 if os.path.isdir(photos_dir):
-#                     for fname in sorted(os.listdir(photos_dir)):
-#                         if os.path.isfile(os.path.join(photos_dir, fname)):
-#                             photos.append(f"/uploads/cars/2/{fname}")
-#
-#                 car2 = Car(
-#                     id=2,
-#                     name="MB CLA45s",
-#                     gps_id="800212421",
-#                     gps_imei="866011056074131",
-#                     engine_volume=2.0,
-#                     year=2019,
-#                     drive_type=3,
-#                     price_per_minute=140,
-#                     price_per_hour=5600,
-#                     price_per_day=100000,
-#                     plate_number="666AZV02",
-#                     latitude=43.224048,
-#                     longitude=76.961871,
-#                     fuel_level=40,
-#                     course=23,
-#                     owner_id=owner.id,
-#                     description="Разбита левая передняя фара. Разбит задний правый фонарь. Вмятина и царапина на правой задней двери.",
-#                     photos=photos
-#                 )
-#                 db.add(car2)
-#                 db.commit()
-#                 print("✅ MB CLA45s (id=2) добавлена")
-#             else:
-#                 print("ℹ️ MB CLA45s (id=2) уже существует")
-#
-#             # Теперь добавляем моковые машины
-#             create_mock_cars_for_almaty(db)
-#
-#             mechanic_phone = "77007007070"
-#             mechanic = db.query(User).filter(User.phone_number == mechanic_phone).first()
-#             if not mechanic:
-#                 mechanic = User(phone_number=mechanic_phone, role=UserRole.MECHANIC, wallet_balance=0)
-#                 db.add(mechanic)
-#                 db.commit()
-#                 db.refresh(mechanic)
-#                 print("✅ Механик успешно добавлен")
-#             else:
-#                 print("ℹ️ Механик уже существует")
-#
-#         except Exception as e:
-#             print(e)
-#             logger.error(f"Ошибка в стартап-инициализации: {e}")
-#         finally:
-#             db.close()
-#
-#         try:
-#             # scheduler.add_job(check_vehicle_conditions, "interval", seconds=1)
-#             scheduler.start()
-#         except Exception as e:
-#             logger.error(f"Ошибка запуска планировщика задач: {e}")
-#
-#     @app.on_event("shutdown")
-#     async def shutdown_event():
-#         print("🛑 Приложение остановлено")
-#         try:
-#             scheduler.shutdown()
-#         except Exception as e:
-#             logger.error(f"Ошибка остановки планировщика: {e}")
-#
+
+def init_app(app: FastAPI):
+    @app.on_event("startup")
+    async def startup_event():
+        print("🚀 Приложение запущено")
+        run_migrations()
+
+        db_gen = get_db()
+        db = next(db_gen)
+        scheduler.add_job(
+            billing_job,
+            trigger="interval",
+            seconds=10,
+            max_instances=1,  # не запустит новую итерацию, пока старая не завершилась
+            coalesce=True  # если промедлили — слить «пропущенные» вызовы в один
+        )
+        scheduler.start()
+
+        try:
+            owner_phone = "77000250400"
+            owner = db.query(User).filter(User.phone_number == owner_phone).first()
+            if not owner:
+                owner = User(phone_number=owner_phone, role=UserRole.FIRST, wallet_balance=0)
+                db.add(owner)
+                db.commit()
+                db.refresh(owner)
+
+            if not db.query(Car).filter(Car.id == 1).first():
+                photos_dir = os.path.join(os.path.dirname(__file__), "uploads", "cars", "1")
+                photos = []
+                if os.path.isdir(photos_dir):
+                    for fname in sorted(os.listdir(photos_dir)):
+                        if os.path.isfile(os.path.join(photos_dir, fname)):
+                            photos.append(f"/uploads/cars/1/{fname}")
+
+                car1 = Car(
+                    id=1,
+                    name="HAVAL F7x",
+                    gps_id="800153076",
+                    gps_imei="866011056063951",
+                    engine_volume=2.0,
+                    year=2021,
+                    drive_type=3,
+                    price_per_minute=70,
+                    price_per_hour=3125,
+                    price_per_day=50000,
+                    plate_number="422ABK02",
+                    latitude=43.238949,
+                    longitude=76.889709,
+                    fuel_level=80,
+                    owner_id=owner.id,
+                    course=90,
+                    description="Машина в идеальном состоянии.",
+                    photos=photos
+                )
+                db.add(car1)
+                db.commit()
+                print("✅ HAVAL F7x (id=1) добавлена")
+            else:
+                print("ℹ️ HAVAL F7x (id=1) уже существует")
+
+            if not db.query(Car).filter(Car.id == 2).first():
+                photos_dir = os.path.join(os.path.dirname(__file__), "uploads", "cars", "2")
+                photos = []
+                if os.path.isdir(photos_dir):
+                    for fname in sorted(os.listdir(photos_dir)):
+                        if os.path.isfile(os.path.join(photos_dir, fname)):
+                            photos.append(f"/uploads/cars/2/{fname}")
+
+                car2 = Car(
+                    id=2,
+                    name="MB CLA45s",
+                    gps_id="800212421",
+                    gps_imei="866011056074131",
+                    engine_volume=2.0,
+                    year=2019,
+                    drive_type=3,
+                    price_per_minute=140,
+                    price_per_hour=5600,
+                    price_per_day=100000,
+                    plate_number="666AZV02",
+                    latitude=43.224048,
+                    longitude=76.961871,
+                    fuel_level=40,
+                    course=23,
+                    owner_id=owner.id,
+                    description="Разбита левая передняя фара. Разбит задний правый фонарь. Вмятина и царапина на правой задней двери.",
+                    photos=photos
+                )
+                db.add(car2)
+                db.commit()
+                print("✅ MB CLA45s (id=2) добавлена")
+            else:
+                print("ℹ️ MB CLA45s (id=2) уже существует")
+
+            # Теперь добавляем моковые машины
+            create_mock_cars_for_almaty(db)
+
+            mechanic_phone = "77007007070"
+            mechanic = db.query(User).filter(User.phone_number == mechanic_phone).first()
+            if not mechanic:
+                mechanic = User(phone_number=mechanic_phone, role=UserRole.MECHANIC, wallet_balance=0)
+                db.add(mechanic)
+                db.commit()
+                db.refresh(mechanic)
+                print("✅ Механик успешно добавлен")
+            else:
+                print("ℹ️ Механик уже существует")
+
+        except Exception as e:
+            print(e)
+            logger.error(f"Ошибка в стартап-инициализации: {e}")
+        finally:
+            db.close()
+
+        try:
+            # scheduler.add_job(check_vehicle_conditions, "interval", seconds=1)
+            scheduler.start()
+        except Exception as e:
+            logger.error(f"Ошибка запуска планировщика задач: {e}")
+
+    @app.on_event("shutdown")
+    async def shutdown_event():
+        print("🛑 Приложение остановлено")
+        try:
+            scheduler.shutdown()
+        except Exception as e:
+            logger.error(f"Ошибка остановки планировщика: {e}")
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -290,7 +290,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# init_app(app)
+init_app(app)
 app.include_router(Auth_router)
 app.include_router(Vehicle_Router)
 app.include_router(RentRouter)
