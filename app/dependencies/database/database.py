583a489c8659d71ffdae8e8f589bb8962b886ae8
Aruzhan Tapisheva <220103262@stu.sdu.edu.kz>
2025-06-23 14:13:36 +0500

    delete while true

--- app/dependencies/database/database.py
+++ app/dependencies/database/database.py
@@ -4,7 +4,14 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=20,  # одновременно до 20 «живых» соединений
+    max_overflow=30,  # ещё до 30 «переполнений» за пределы pool_size
+    pool_timeout=30,  # ждать соединение не более 30 секунд
+    pool_pre_ping=True,  # проверять «живость» соединения перед выдачей
+    pool_recycle=1800,  # перезапускать соединение через 30 минут
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
