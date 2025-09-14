ff8a230cc51425d8f3fc48ced23ac852c69020be
Karina Kim <220103048@stu.sdu.edu.kz>
2025-09-14 17:19:43 +0500

    fix

--- app/models/init.py
+++ app/models/init.py
@@ -1,3 +1,5 @@
 from app.models.user_model import User, UserRole, AutoClass
 from app.models.car_model import Car
 from app.models.guarantor_model import GuarantorRequest, GuarantorRequestStatus, Guarantor, ContractFile, VerificationStatus
+from app.models.history_model import RentalHistory, RentalStatus, RentalType, RentalReview
+from app.models.rental_actions_model import RentalAction, ActionType
