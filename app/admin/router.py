ead1c7eedc10521be5e7272a4f31dd5ba29aa6dc
Karina Kim <220103048@stu.sdu.edu.kz>
2025-10-04 02:09:02 +0500

    fix

--- app/admin/router.py
+++ app/admin/router.py
@@ -3,7 +3,7 @@ from app.admin.cars.router import cars_router
 from app.admin.guarantors.router import guarantors_router
 from app.admin.users.router import users_router
 
-admin_router = APIRouter(prefix="/admin", tags=["Admin"])
+admin_router = APIRouter(prefix="/admin")
 
 admin_router.include_router(cars_router, prefix="/cars")
 admin_router.include_router(guarantors_router, prefix="/guarantors")
