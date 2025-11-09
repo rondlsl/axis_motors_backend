545b803aec4079dad8d2873df7adaeab4f601e24
Aruzhan Tapisheva <220103262@stu.sdu.edu.kz>
2025-11-09 16:05:28 +0500

    fix bugs

--- app/rent/exceptions.py
+++ app/rent/exceptions.py
@@ -3,9 +3,11 @@ from fastapi import HTTPException
 
 class InsufficientBalanceException(HTTPException):
     def __init__(self, required_amount: int):
+        formatted_amount = f"{required_amount:,}".replace(",", " ")
         super().__init__(
             status_code=402,
             detail=(
-                f"Пожалуйста, пополните счёт: для бронирования необходимо минимум {required_amount}₸."
+                f"Для аренды автомобиля на вашем балансе должна быть минимальная сумма {formatted_amount} ₸.\n\n"
+                f"Оплата поездки списывается по фактическому использованию автомобиля, оставшиеся средства останутся на вашем балансе."
             )
         )
